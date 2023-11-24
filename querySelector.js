3351b6c4361c80f2b49a52b78698446412a1be0b
phonkedout <149279801+phonkedout@users.noreply.github.com>
2023-11-24 15:47:51 +1100

    Create querySelector.js

---
+++ querySelector.js
@@ -0,0 +1,17 @@
+/*
+Basic querySelector 
+
+Complete the method getFooterElement such that it finds and returns the element with the id footer-wrapper from the HTML page.
+Make sure to take a look at the code in the index.html file.
+*/
+
+const getFooterElement = () => {
+  return document.querySelector("#footer-wrapper");
+}
+
+// Sample usage - do not modify
+console.log(getFooterElement()); 
+}
+
+// Sample usage - do not modify
+console.log(getFooterElement());
